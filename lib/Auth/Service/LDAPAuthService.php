a7c9ef196f59b2f09c8659451f58743349e46e91
John Roepke <john@justjohn.us>
2010-10-18 01:03:15 -0400

    Added caching to Auth manager and updates to LDAP auth service

--- lib/Auth/Service/LDAPAuthService.php
+++ lib/Auth/Service/LDAPAuthService.php
@@ -229,18 +229,17 @@ class LDAPAuthService implements AuthService, AuthServiceSubgroups {
         $ldap_attr["objectclass"][1] = $attributes->groupObjectClass;
         $ldap_attr[$attributes->groupNameAttr] = $name;
         $ldap_attr[$attributes->groupDescAttr] = $description;
+        $ldap_attr[$attributes->groupMemberAttr] = '';
 
-        if (count($users) > 0) {
+        /* if (count($users) > 0) {
+            $ldap_attr[$attributes->groupMemberAttr] = array();
             foreach ($users as $user) {
                 $userdn = $attributes->usernameAttr . "=" . $user . "," . $attributes->userDN . "," . $attributes->baseDN;
                 $ldap_attr[$attributes->groupMemberAttr][] = $userdn;
             }
-        } else {
-            $ldap_attr[$attributes->groupMemberAttr] = '';
-        }
+        } */
 
         $groupdn = $attributes->groupNameAttr . "=" . $name . "," . $attributes->groupDN . "," . $attributes->baseDN;
-        //print("ldap_add(\$this->ldap_conn, '$groupdn', ".get_a($ldap_attr).")<br />");
         $result = @ldap_add($this->ldap_conn, $groupdn, $ldap_attr);
         return $result;
 
