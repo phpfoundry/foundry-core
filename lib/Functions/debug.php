b6d3c64899ef7fb70e04a7287efe9190e3680896
John Roepke <john@justjohn.us>
2010-10-27 13:08:02 -0400

    Removed krumo call from debug functions

--- lib/Functions/debug.php
+++ lib/Functions/debug.php
@@ -4,8 +4,6 @@
  * @package functions
  */
 
-include("krumo/class.krumo.php");
-
 /**
  * Print a textual representation of an array or object.
  * @param object $arr
@@ -21,7 +19,7 @@ function print_a($arr) {
  */
 function get_a($arr) {
     ob_start();
-    krumo($arr);
+    var_dump($arr);
     $content = ob_get_contents();
     ob_end_clean();
     return $content;
